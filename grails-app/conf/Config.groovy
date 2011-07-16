936e6d3e807b8271cd487a21d11965c83ee9a88a
Daniel H A Lima <email_daniel_h@yahoo.com.br>
2011-07-16 12:05:22 -0300

    Fixed test.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -22,7 +22,7 @@ log4j = {
 
     warn   'org.mortbay.log'
     
-    debug  'org.grails.plugins.freemarker'
+    //debug  'org.grails.plugins.freemarker'
 }
 
 // The following properties have been added by the Upgrade process...
--- test/integration/org/grails/plugins/freemarker/ConcurrencyTests.groovy
+++ test/integration/org/grails/plugins/freemarker/ConcurrencyTests.groovy
@@ -27,16 +27,20 @@ class ConcurrencyTests extends GroovyTestCase {
 
     def freemarkerConfig
     private Exception threadException
-    private ThreadGroup myThreadGroup = new ThreadGroup('x') {public void uncaughtException(Thread t,Throwable e) {super.uncaughtException(t, e); threadException = e}}
-    
+    private ThreadGroup myThreadGroup = new ThreadGroup('x') {
+        public void uncaughtException(Thread t,Throwable e) {
+            super.uncaughtException(t, e); threadException = e
+        }
+    }
+
     protected void setUp() {
         super.setUp(); threadException = null
     }
 
     protected void tearDown() {
         super.tearDown()
-    }    
-    
+    }
+
     void testConcurrency() {
         def text = new StringBuilder('[#ftl/]')
         for (int i = 0; i < 1000; i++) {
@@ -46,28 +50,45 @@ class ConcurrencyTests extends GroovyTestCase {
         Configuration cfg = freemarkerConfig.configuration
         Template template = new Template('template', new StringReader(text.toString()), cfg)
         text = null
-        
-        for (int counter = 0; counter < 20; counter++) {
-            Thread.currentThread().yield()
-            runInParallel {
+
+        Thread mainThread = Thread.currentThread()
+        List threads = []
+        for (int counter = 0; counter < 30; counter++) {
+            Thread t = runInParallel {
+                final int myCounter = counter
                 StringWriter result = new StringWriter()
-                template.process([name: counter + 1], result)
-                Thread.currentThread().yield()
+                template.process([name: myCounter + 1], result)
                 List lines = new StringReader(result.toString()).readLines(); result = null
-                assertEquals "thread ${counter} ${lines.size()}", 1000, lines.size()
-                lines.eachWithIndex {line, index ->
-                    //if (Math.random() <= 0.1) {assertTrue 'random', false}
-                    assertEquals "${index}: Thread-${counter+1}".toString(), line 
+                assertEquals "thread ${myCounter} ${lines.size()}", 1000, lines.size()
+                lines.eachWithIndex { line, index ->
+                    if (Math.random() <= 0.1) {
+                        //assertTrue "random ${index} ${myCounter+1}", false
+                    }
+                    assertEquals "${index}: Thread-${myCounter+1}".toString(), line
                 }
             }
-            Thread.currentThread().yield()
+            threads << t
         }
-        
+
+        if (Thread.currentThread() == mainThread) {
+            threads.eachWithIndex {thread, idx ->
+                while (thread.isAlive() && threadException == null) {
+                    Thread.sleep 1000
+                }
+                
+                println "Exiting ${idx + 1}: ${thread} ${System.currentTimeMillis()}"
+            }
+            if (threadException != null) {
+                throw threadException
+            }
+        }
     }
-    
-    private runInParallel = {Closure c ->
-        if (threadException != null) {throw threadException}
-        def thread = null; boolean executed = false 
+
+    private runInParallel = { Closure c ->
+        if (threadException != null) {
+            throw threadException
+        }
+        def thread = null; boolean executed = false
         Closure c1 = {
             try {
                 c()
@@ -75,12 +96,9 @@ class ConcurrencyTests extends GroovyTestCase {
                 executed = true
             }
         }
-        
-        thread = new Thread(myThreadGroup, c1 as Runnable); thread.daemon = true; thread.start(); thread.yield()
-        while (thread.isAlive() && !executed) {
-            Thread.sleep 1000
-        }
-        if (threadException != null) {throw threadException}
+
+        thread = new Thread(myThreadGroup, c1 as Runnable); //thread.daemon = true;
+        thread.start(); thread.yield()
+        return thread
     }
-    
 }
